f96b6bb02677a37a5fc73698f86841dbf6d38dd7
JonathanYe <jmye@umich.edu>
2016-10-20 22:17:55 -0400

    added additional logging

--- rpn.py
+++ rpn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import operator
-
+import logging
 
 operators = {
 	'+': operator.add,
@@ -24,7 +24,7 @@ def calculate(myarg):
 			arg1 = stack.pop()
 			result = function(arg1, arg2)
 			stack.append(result)
-		print(stack)
+		logging.debug(stack)
 	if len(stack) != 1:
 		raise TypeError("Too many parameters")
 	return stack.pop()
@@ -33,7 +33,7 @@ def helloWorld():
 def main():
 	while True:
 		result = calculate(input("rpn calc> "))
-		print("Result: ", result)
+		logging.info("Result: ", result)
 		print("Hello World!")
 
 if __name__ == '__main__':
